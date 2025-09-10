4e451df9bc05fb8766796a48ee2ce225a7bed8c9
Guilherme Godoy <guilhermehlgodoy22@gmail.com>
2025-09-10 16:14:48 +0100

    feat(publication): add copyright fields to issue headers and processor
    Add copyrightYear, copyrightHolder, and licenseUrl fields to required issue headers and implement logic in PublicationProcessor to set these values from system defaults when not provided. Update documentation and example files to reflect new fields.

--- classes/validations/RequiredIssueHeaders.php
+++ classes/validations/RequiredIssueHeaders.php
@@ -47,6 +47,9 @@ class RequiredIssueHeaders
         'datePublished',
         'startPage',
         'endPage',
+        'copyrightYear',
+		'copyrightHolder',
+		'licenseUrl',
     ];
 
     static $issueRequiredHeaders = [
